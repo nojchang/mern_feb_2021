d96230bb2ed10d0683ad93ad660ec5e1c3b35716
Jon Chang <jonchang19@gmail.com>
2021-04-28 00:44:00 -0700

    Tabs, create tab headers, when clicked on correctly show the appropriate content below.

---
+++ week_3/tabs/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+import React, { useState } from 'react';
+import Tabs from './components/Tabs';
+import Results from './components/Results';
+
+function App() {
+
+  const tabsArray = [
+    { label: "Tab 1", content: "Tab 1 content is showing here"},
+    { label: "Tab 2", content: "Tab 2 content is showing here"},
+    { label: "Tab 3", content: "Tab 3 content is showing here"},
+    { label: "Tab 4", content: "Tab 4 content is showing here"},
+  ];
+
+  const [ allTabs, setAllTabs ] = useState(tabsArray);
+
+  const [ currentTabIndex, setCurrentTabIndex ] = useState(0);
+
+  return (
+    <div className="App">
+      <Tabs
+      allTabs={ allTabs }
+      currentTabIndex={ currentTabIndex }
+      setCurrentTabIndex= { setCurrentTabIndex }
+      />
+      <Results allTabs= { allTabs } currentTabIndex={ currentTabIndex } />
+       </div>
+      
+  );
+}
+
+export default App;
